f2c193d6ac063ba40b9a211b4eb757652b46c2ac
odd-rabbit <64193822+odd-rabbit@users.noreply.github.com>
2022-04-07 23:16:30 +0800

    edit test of rest_mfc

--- source/rest_mfc.py
+++ source/rest_mfc.py
@@ -1,8 +1,7 @@
-import json
 import numpy as np
 import requests
 import pandas as pd
-import time
+
 
 class API(object):
     def __init__(self):
@@ -86,17 +85,16 @@ class API(object):
 if __name__ == '__main__':
     import time
 
-    # API().price('bitcoin')
+    # API().price('bitcoin').to_csv('tickers.csv')
     API().tickers(['BTC_USDT']).to_csv('tickers.csv')
     # API().kline('BTC_USDT', '1m').to_csv('kline.csv')
-    # API().orderbook('BTC_USDT', 100).to_csv('book.csv', header=True)
-    # API().trades('BTC_USDT', 100).to_csv('trade.csv', header=True)
-    while True:
+    # API().orderbook('BTC_USDT', 100).to_csv('book.csv')
+    # API().trades('BTC_USDT', 100).to_csv('trade.csv')
+    for i in range(5):
         time.sleep(1)
-        # print(API().price('bitcoin'))
-        API().tickers(['BTC_USDT']).to_csv('tickers.csv', mode='a')
+        # API().price('bitcoin').to_csv('price.csv', mode='a', header=False)
+        API().tickers(['BTC_USDT']).to_csv('tickers.csv', mode='a', header=False)
         # API().kline('BTC_USDT', '1m').to_csv('kline.csv', mode='a', header=False)
         # API().trades('BTC_USDT', 100).to_csv('trade.csv', mode='a', header=False)
         # API().orderbook('BTC_USDT', 100).to_csv('book.csv', mode='a', header=False)
-
-        print('1')
+        print(i)
